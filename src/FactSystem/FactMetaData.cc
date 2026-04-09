4c9826d686824cfd3567118edf75a690b7717336
Julian Oes <julian@oes.ch>
2026-04-09 13:09:58 +1200

    Decouple volatile from read-only and fix parameter list layout
    - Remove _readOnly = true from setVolatileValue() since volatile params
      (e.g. calibration offsets) should still be user-writable
    - Fix description column width being cut off due to wrong implicitWidth
    - Fix QML warning about assigning Fact to QString in name column

--- src/FactSystem/FactMetaData.cc
+++ src/FactSystem/FactMetaData.cc
@@ -1480,9 +1480,6 @@ QVariant FactMetaData::cookedMin() const
 void FactMetaData::setVolatileValue(bool bValue)
 {
     _volatile = bValue;
-    if (_volatile) {
-        _readOnly = true;
-    }
 }
 
 QStringList FactMetaData::splitTranslatedList(const QString &translatedList)
